9635f91b93461d9eef6bd0577bd3e3f67c0f14d4
Mario Cezzare <mario.chiodi@catho.com>
2024-05-11 15:38:16 -0300

    Initial Commit

---
+++ WhatIsThis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatIsThis
+//
+//  Created by Mario Chiodi on 11/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
