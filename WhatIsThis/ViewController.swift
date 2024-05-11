0e36b1ad46c0bf68cabc04aa9ff9f2b55a6f6d99
Mario Cezzare <mario.chiodi@catho.com>
2024-05-11 16:14:39 -0300

    take picture working

--- WhatIsThis/ViewController.swift
+++ WhatIsThis/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var imagePicker: UIImagePickerController!
+    
+    // MARK: - Outlets
+        
+    @IBOutlet var imageView: UIImageView!
 
+    
+    @IBAction func takePicture(_ sender: UIButton) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker,animated:true,completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // actions
 
+    
+    // MARK: - Delegates
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePicker.dismiss(animated: true,completion:nil)
+        imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+    }
 }
 
